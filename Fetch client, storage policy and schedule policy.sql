98e9467142c70801a2a8b03a2f82fabcd52b31c0
Harshavardhan Nara <48521516+naraharsha224@users.noreply.github.com>
2025-01-24 00:54:44 +0530

    Update Fetch client, storage policy and schedule policy.sql

--- Fetch client, storage policy and schedule policy.sql
+++ Fetch client, storage policy and schedule policy.sql
@@ -1,18 +1,37 @@
 --I've created this query to do pre and post quality checks while doing Commcell client migrations 
 
-SELECT DISTINCT 
-    CBSS.clientName,
-    CBSS.idaagent,
-    CBSS.backupset,
-    CBSS.subclient,
-    CSP.storagepolicy,
-    CBSS.scheduePolicy,
-    CBSS.schedbackuptype,
-    CBSS.schedbackupTime
-FROM CommCellBkScheduleForSubclients CBSS
-INNER JOIN APP_Client C ON C.name = CBSS.clientName
-INNER JOIN APP_ClientGroupAssoc CGA ON C.id = CGA.clientId
-INNER JOIN APP_ClientGroup CG ON CG.id = CGA.clientGroupId
-INNER JOIN CommCellStoragePolicy CSP ON CSP.clientname = C.name
-WHERE CG.name = 'CS401-MoveGroup-EverythingElse-02' --Give the groupName as required
-      AND CSP.storagepolicy != 'CV_DEFAULT';
+USE commserv;
+WITH processedData AS (
+SELECT 
+        CBSS.clientName,
+        CBSS.idaagent,
+        CBSS.backupset,
+        CBSS.subclient,
+        CSP.storagepolicy,
+        CBSS.scheduePolicy,
+        CBSS.scheduleName,
+        CBSS.schedbackuptype,
+        CBSS.schedbackupTime,
+        ROW_NUMBER() OVER (PARTITION BY CBSS.clientName, CBSS.subclient, schedbackuptype ORDER BY CBSS.schedbackupTime DESC
+        ) AS row_num
+    FROM CommCellBkScheduleForSubclients CBSS
+    JOIN APP_Client C ON C.name = CBSS.clientName
+    JOIN APP_ClientGroupAssoc CGA ON C.id = CGA.clientId
+    JOIN APP_ClientGroup CG ON CG.id = CGA.clientGroupId
+    JOIN CommCellStoragePolicy CSP ON CSP.clientName = C.name
+    WHERE CG.name = 'CS803-MoveGroup-Cluster-5' -- Specify the groupName as required
+      AND CSP.storagepolicy != 'CV_DEFAULT'
+)
+SELECT 
+    clientName,
+    idaagent,
+    backupset,
+    subclient,
+    storagepolicy,
+    scheduePolicy,
+    scheduleName,
+    schedbackuptype,
+    schedbackupTime
+FROM processedData
+WHERE row_num = 1
+ORDER BY clientName, subclient;
